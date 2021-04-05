d6ee65bfee5857c593a90740f1beee287a5ccf49
Ghulam Ali <ghulamalimohtaj@gmail.com>
2021-04-05 13:56:24 +0430

    Doing form validation

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: true,
     },
     body: {
         type: String,
--- public/js/myjs.js
+++ public/js/myjs.js
@@ -1,9 +1,32 @@
-$(document).ready(function(){
-    $('button').click(function(){
-        var id = $('#blogId').val();
-        $.ajax({
-            type: 'DELETE',
-            url:'/'+id
-        });
-    });
-});
+function validate() {
+    var author = document.getElementById('author');
+    var t = document.getElementById('title');
+    var body = document.getElementById('body');
+    var titleFlag = true;
+    var authorFlag = true;
+    var bodyFlag = true;
+
+ 
+    let titleRegex = /[0-9!@#$%^&*+=/.>?{}<]/ig;
+    let authorRegex = /[^A-z]/ig;
+    if(t.value.match(titleRegex) == null){
+        titleFlag = false
+        document.getElementById('titleErr').innerHTML = "Punctuation is not allowed for title";
+    }else{
+        titleFlag = true;
+    }
+    if(author.value.match(authorRegex) == null){
+        authorFlag = true;
+    }else{
+        authorFlag = false;
+        document.getElementById('authorErr').innerHTML="Author must contain only alphabet letters!";
+    }
+    if(body.value.length>20){
+        bodyFlag = false
+        document.getElementById('bodyErr').innerHTML="Body can't exceed 20 words!";
+    }else{
+        bodyFlag = true;
+    }
+
+    return authorFlag && bodyFlag && titleFlag;
+}
